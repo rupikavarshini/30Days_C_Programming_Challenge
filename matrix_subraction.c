4aa9941f122c1f55bbaac2d386c36d01f053920a
rupikavarshini <rupikavarshini@gmail.com>
2026-08-02 09:43:55 +0530

    Add files via upload

---
+++ matrix_subraction.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+void main(){
+    int num[3][3],i,j,arr[3][3],add[3][3];
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+    printf("enter ur number");
+    scanf("%d",&num[i][j]);
+    }
+   }
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+    printf("%d",num[i][j]);
+    printf("\t");
+    }
+    printf("\n");
+  }
+  for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+    printf("enter ur number");
+    scanf("%d",&arr[i][j]);
+    }
+  }
+   for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+    printf("%d",arr[i][j]);
+    printf("\t");
+    }
+    printf("\n");
+  }
+  for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+   add[i][j]=arr[i][j]-num[i][j];
+    }
+  }
+  printf("\tSUBRACTION\n");
+  for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+    printf("%d",add[i][j]);
+    printf("\t");
+    }
+    printf("\n");
+   }
+
+}
